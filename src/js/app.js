dafebeb6265b591ac2f61e8b369111161ebd41ee
DenisTis <tislenko.denis@gmail.com>
2017-08-20 18:05:59 +0200

    cannon physics added

--- src/js/app.js
+++ src/js/app.js
@@ -4,62 +4,119 @@
 // require('script-loader!three/build/three.min.js');
 // require('script-loader!three-obj-loader/dist/index.js');
 
-var THREE = require('three');
-var OrbitControls = require('three-orbit-controls')(THREE);
-// var OBJLoader = require('three-obj-loader');
-// OBJLoader(THREE);
-// var MTLLoader = require('three-mtl-loader');
+const THREE = require('three');
+const OrbitControls = require('three-orbit-controls')(THREE);
+const CANNON = require('cannon');
 
-// Create an empty scene
-var scene = new THREE.Scene();
-scene.background = new THREE.Color(0xffffff);
+//TODO: convert it to return new object
+require('./KeyboardState');
+//const KEYBOARD = require('./KeyboardState');
+const TIMESTEP = 1 / 60;
 
-// Create a basic perspective camera
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 4;
+let keyboard, clock, world, scene, camera, renderer, controls, model, pModel;
 
-let controls = new OrbitControls(camera);
+keyboard = new KeyboardState();
+initCannon();
+addPModel();
+initThree();
+loadModel();
 
-// Create a renderer with Antialiasing
-var renderer = new THREE.WebGLRenderer({ antialias: true });
-// Configure renderer clear color
-renderer.setClearColor("#000000");
-// Configure renderer size
-renderer.setSize(window.innerWidth, window.innerHeight);
-// Append Renderer to DOM
-document.body.appendChild(renderer.domElement);
+function initCannon() {
+    world = new CANNON.World();
+    world.gravity.set(0, -0.1, 0);
+    world.broadphase = new CANNON.NaiveBroadphase();
+    world.solver.iterations = 10;
+}
 
-// ------------------------------------------------
-// FUN STARTS HERE
-// ------------------------------------------------
+function initThree() {
+    // Create an empty scene
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xffffff);
 
-//Provide lights to load the model
-var ambient = new THREE.AmbientLight(0x101030);
-scene.add(ambient);
-var directionalLight = new THREE.DirectionalLight(0xffeedd);
-directionalLight.position.set(0, 0, 1);
-scene.add(directionalLight);
+    // Create a basic perspective camera
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.z = 4;
 
+    controls = new OrbitControls(camera);
 
-//Load model
-var model;
+    // Create a renderer with Antialiasing
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    // Configure renderer clear color
+    renderer.setClearColor("#000000");
+    // Configure renderer size
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    // Append Renderer to DOM
+    document.body.appendChild(renderer.domElement);
 
-var jLoader = new THREE.JSONLoader();
-jLoader.load('assets/models/simpleCar/test.json', function(geometry, materials) {
-    let object = new THREE.Mesh(geometry, new THREE.MeshFaceMaterial(materials));
-    object.position.z = -5;
-    object.rotation.x = 0.5;
-    model = object;
-    scene.add(object);
-    render();
-    //    checkLoaded();
-});
+    //Provide lights to load the model
+    var ambient = new THREE.AmbientLight(0x101030);
+    scene.add(ambient);
+    var directionalLight = new THREE.DirectionalLight(0xffeedd);
+    directionalLight.position.set(0, 0, 1);
+    scene.add(directionalLight);
+}
 
-// Render Loop
-var render = function() {
-    requestAnimationFrame(render);
-    model.rotation.x += 0.005;
-    model.rotation.y += 0.005;
-    // Render the scene
+function addPModel() {
+    let shape = new CANNON.Box(new CANNON.Vec3(1, 1, 1));
+    let mass = 1;
+    pModel = new CANNON.Body({ mass: mass });
+    pModel.position = new CANNON.Vec3(0, 0, -5);
+    pModel.addShape(shape);
+    pModel.angularVelocity.set(0, 1, 0);
+    pModel.angularDamping = 0.5;
+    world.add(pModel);
+}
+
+function loadModel() {
+    var jLoader = new THREE.JSONLoader();
+    jLoader.load('assets/models/simpleCar/test.json', function(geometry, materials) {
+        let object = new THREE.Mesh(geometry, new THREE.MeshFaceMaterial(materials));
+        object.position.z = -5;
+        object.rotation.x = 0.5;
+        model = object;
+        scene.add(object);
+        animate();
+    });
+}
+
+function animate() {
+    clock = new THREE.Clock();
+    requestAnimationFrame(animate);
+    updatePhysics();
+    // model.rotation.x += 0.005;
+    // model.rotation.y += 0.005;
     renderer.render(scene, camera);
-};
+    updateUserInput();
+}
+
+function updatePhysics() {
+    world.step(TIMESTEP);
+    model.position.copy(pModel.position);
+    //model.quaternion.copy(pModel.position);
+}
+
+function updateUserInput() {
+    keyboard.update();
+
+    // if (keyboard.pressed("left") || keyboard.pressed("A")) {
+    //     let moveDistance = 1 * clock.getDelta();
+    //     car.rotation.y += moveDistance;
+    // }
+
+
+    // if (keyboard.pressed("right") || keyboard.pressed("D")) {
+    //     let moveDistance = 0.7 * clock.getDelta();
+    //     car.rotation.y -= moveDistance;
+    // }
+
+    if (keyboard.pressed("up") || keyboard.pressed("W")) {
+        let speed = clock.getDelta();
+        console.log("speed " + speed);
+        pModel.velocity.y += 0.01;
+
+    }
+
+    if (keyboard.pressed("down") || keyboard.pressed("S")) {
+        pModel.velocity.y -= 0.01;
+    }
+}
